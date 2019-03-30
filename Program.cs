0d59325306f79fe3abbbe96fa09da5b5716fe798
gean-xyz <me@gean.at>
2019-03-30 13:01:03 +0100

    v

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ namespace FibonacciSeries {
 
     public static int[] CreateFibonacciSeries(int count) {
       int[] fibonacci = new int[count];
-      fibonacci[1] = 1;
       fibonacci[0] = 0;
+      fibonacci[1] = 1;
 
       for (int i = 2; i < count; i++) {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
