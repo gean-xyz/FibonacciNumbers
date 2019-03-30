877eecbfa6b16276995f84ee2ff4a7c3a8d77047
gean-xyz <me@gean.at>
2019-03-30 13:00:03 +0100

    changed indexes

--- Program.cs
+++ Program.cs
@@ -18,8 +18,8 @@ namespace FibonacciSeries {
 
     public static int[] CreateFibonacciSeries(int count) {
       int[] fibonacci = new int[count];
-      fibonacci[0] = 0;
       fibonacci[1] = 1;
+      fibonacci[0] = 0;
 
       for (int i = 2; i < count; i++) {
         fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
